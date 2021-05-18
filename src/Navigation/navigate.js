ce904fc6acf04614c3aec981a38a8fdbb281fc92
thangavel-21 <thangavelganesan21@gmail.com>
2021-05-18 20:51:35 +0530

    changes navigator

--- src/Navigation/navigate.js
+++ src/Navigation/navigate.js
@@ -9,48 +9,43 @@ import signup from '../signup/signup';
 import forgot from '../forgot/forgot';
 import image from '../updates/image';
 
-
 const Stack = createStackNavigator();
 
 function navigate() {
   return (
     <NavigationContainer>
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <Stack.Navigator initialRouteName={Navkeys.FRONT}>
-            <Stack.Screen
-              name={Navkeys.FRONT}
-              options={{headerShown: false}}
-              component={front}
-            />
-            <Stack.Screen
-              name={Navkeys.LOGIN}
-              options={{headerShown: false}}
-              component={Login}
-            />
-            <Stack.Screen
-              name={Navkeys.IMAGE}
-              options={{headerShown: false}}
-              component={image}
-            />
-            <Stack.Screen
-              name={Navkeys.FORGOT}
-              options={{headerShown: false}}
-              component={forgot}
-            />
-            <Stack.Screen
-              name={Navkeys.NEXTPAGE}
-              options={{headerShown: false}}
-              component={next}
-            />
-            <Stack.Screen
-              name={Navkeys.SIGNUP}
-              options={{headerShown: false}}
-              component={signup}
-            />
-          </Stack.Navigator>
-        </PersistGate>
-      </Provider>
+      <Stack.Navigator initialRouteName={Navkeys.FRONT}>
+        <Stack.Screen
+          name={Navkeys.FRONT}
+          options={{headerShown: false}}
+          component={front}
+        />
+        <Stack.Screen
+          name={Navkeys.LOGIN}
+          options={{headerShown: false}}
+          component={Login}
+        />
+        <Stack.Screen
+          name={Navkeys.IMAGE}
+          options={{headerShown: false}}
+          component={image}
+        />
+        <Stack.Screen
+          name={Navkeys.FORGOT}
+          options={{headerShown: false}}
+          component={forgot}
+        />
+        <Stack.Screen
+          name={Navkeys.NEXTPAGE}
+          options={{headerShown: false}}
+          component={next}
+        />
+        <Stack.Screen
+          name={Navkeys.SIGNUP}
+          options={{headerShown: false}}
+          component={signup}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
